50c03695b09c5124a4006468f474b0e1ccc536ca
heldersantiago273 <heldersantiago273@gmail.com>
2024-08-31 02:52:55 +0100

    inital commit

---
+++ src/tasks/tasks.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TaskController } from './tasks.controller';
+import { TaskService } from './tasks.service';
+
+describe('TaskController', () => {
+  let controller: TaskController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [TaskController],
+      providers: [TaskService],
+    }).compile();
+
+    controller = module.get<TaskController>(TaskController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
